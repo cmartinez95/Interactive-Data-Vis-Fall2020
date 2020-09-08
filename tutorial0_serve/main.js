c41cc880d62cf536a46230387849208d33b5630d
Christian Martinez <christianmartinez@Christians-Air.fios-router.home>
2020-09-08 10:55:48 -0400

    maybe this will change the color

--- tutorial0_serve/main.js
+++ tutorial0_serve/main.js
@@ -2,4 +2,6 @@ console.log("La bomba esta aqui")
 console.log("I think doing homework on Labor Day is outlawed")
 console.log(5*5)
 
-
+const div = d3.select('.body') 
+    .style("background-color","pink")
+    
