b90beb131289a6676401e50ea20dd6354e4b4889
Christian Martinez <christianmartinez@Christians-MacBook-Air.local>
2020-10-22 15:16:55 -0400

    ToolTips

--- tutorial3_distributions/Tooltip.js
+++ tutorial3_distributions/Tooltip.js
@@ -118,31 +118,50 @@ function draw() {
   }
 console.log(filteredData)
   const dot = svg
-    .selectAll(".dot")
+    .selectAll(".dotgroup")
     .data(filteredData, d => d.Albums) // use `d.name` as the `key` to match between HTML and data elements
     .join(
       enter =>
         // enter selections -- all data elements that don't have a `.dot` element attached to them yet
         enter
-          .append("circle")
-          .attr("class", "dot") // Note: this is important so we can identify it in future updates
-          .attr("stroke", "lightgrey")
-          .attr("opacity", 0.5)
-          .attr("fill", d => {
-            if (d.Genre === "Rap") return "purple";
-            else if (d.Genre === "Pop") return "orange";
-            // else return "red";
-          })
-          .attr("r", 0)
-          .attr("cy", d => yScale(d.Rating))
-         .attr("cx", d => xScale(d.Sales))
-          .call(enter =>
+          .append("g")
+          .attr("class", "dotgroup") // Note: this is important so we can identify it in future updates
+          .call(enter=>
             enter
-              .transition() // initialize transition
-              // .delay(d => 500 * d.Sales) // delay on each element
-              .duration(500) // duration 500ms
-              .attr("r",10)
-          ),
+            .append("circle")
+            .on("mouseover",function(){
+              d3.select(this.parentNode).classed("hover",true)     //"this" is talking about the circle 
+            })
+            .on("mouseout",function(){
+              d3.select(this.parentNode).classed("hover",false)
+            })
+            .attr("stroke", "lightgrey")
+            .attr("opacity", 0.5)
+            .attr("fill", d => {
+              if (d.Genre === "Rap") return "purple";
+              else if (d.Genre === "Pop") return "orange";
+              // else return "red";
+            })
+            .attr("r", 0)
+            .attr("cy", d => yScale(d.Rating))
+          .attr("cx", d => xScale(d.Sales))
+            .call(enter =>
+              enter
+                .transition() // initialize transition
+                // .delay(d => 500 * d.Sales) // delay on each element
+                .duration(500) // duration 500ms
+                .attr("r",10)
+          ))
+          .call(enter=>
+            enter
+            .append("rect")
+            .attr("stroke", "lightgrey")
+            .attr("opacity", 0)
+            .attr("height",10)
+            .attr("width",10)
+            .attr("y", d => yScale(d.Rating))
+            .attr("x", d => xScale(d.Sales))
+            ),
       update =>
         update.call(update =>
           // update selections -- all data elements that match with a `.dot` element
