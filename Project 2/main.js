1c9287f98b055032f5110cef38652e9f814b559e
Christian Martinez <christianmartinez@Christians-Air.fios-router.home>
2020-12-03 18:40:24 -0500

    Project 2

---
+++ Project 2/main.js
@@ -0,0 +1,11 @@
+// Constants and Globals
+const width = window.innerWidth *0.7,
+    height = window.innerHeight *0.7,
+    margin = { top: 20, bottom: 50, left: 60, right: 50 },
+    radius = 5
+  
+
+// Time to make some variables
+let svg;
+let xScale;
+let yScale;
